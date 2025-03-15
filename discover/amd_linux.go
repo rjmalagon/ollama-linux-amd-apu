1d5ba662f2a4439b9f5d0308bd721b3cfa515fc2
Ricardo Jesus Malagon Jerez <rjmalagon@protonmail.ch>
2025-03-15 11:12:03 -0600

    Experimental duct-taped gfx900 support

--- discover/amd_linux.go
+++ discover/amd_linux.go
@@ -59,6 +59,7 @@ var (
 		"gfx1036", // Generic RDNA2
 		"gfx940",  // MI300A (CDNA3)
 		"gfx90c",  // Radeon Vega 7 (Ryzen 5600G)
+                "gfx900",  // Renoir (Ryzen 4000G)
 	}
 
 	// ApuUseGTT indicates whether GTT memory allocation is enabled for the current APU
